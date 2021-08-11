8c2bc7344e0311fa1c9c77af553904274d1a401f
taniya76 <www.taniyamittalhgn@gmail.com>
2021-08-12 01:10:43 +0530

    Edits

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const _ = require("lodash");
 
 
 const homeStartingContent = "This app can be your friend, here you can add your Daily posts";
-const aboutContent = "Textile manufacturing refinancing is beating. Textile manufacturing dictumst the kids elit. There diameter and boat manufacturing lorem. Consectetur adipiscing elit sagittis purus each one it is. But the price they want, but the smile Vulputate soccer massage. In some salad largest ecological. Makeup is always the laughter from her, Whosoever shall not nibh sed ac hendrerit gravida. Westinghouse peanut sauce or carrots mass of temperature. For the arrows of life, so that the earth element. In basketball largest peanut running Massa developers worth it.";
 const contactContent = "Thermal deductible until the price vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Inspections Ut et drink recipes. Minneapolis developer undergraduate homework et. Laughter pull undergraduate at iaculis in the region. Nor do some shooting movies malesuada bibendum sapien arcu vitae. Recipe sometimes varied mainstream real estate. But now targeted propaganda opportunities. Sometimes put lorem ipsum carrots undergraduate tomato soup. The cushion element of the whole, they shall neither. Basketball was pregnant dark to invest clinical zero. So that the disease in the aliquam sem mauris fringilla tincidunt. Set the temperature to photography always pull for free.";
 
 const app = express();
@@ -43,6 +42,11 @@ app.get("/about", function (req, res) {
 
 app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent });
+});
+
+app.post("/contact",function(req,res){
+    console.log(req.body.contact_mail,req.body.contact_message);
+    res.redirect("/contact");
 });
 
 app.get("/compose", function (req, res) {
